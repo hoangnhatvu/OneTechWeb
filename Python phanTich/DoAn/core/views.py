0538382fb929cb3f9512e027e95f2fa4ea2afd5d
hoangnhatvu35202 <20110751@student.hcmute.edu.vn>
2022-05-08 10:57:43 +0700

    hehehehehe

---
+++ Python phanTich/DoAn/core/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
+from product.models import Product, Category
+
+# Create your views here.
+def store(request,category_slug=None):
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=categories, is_available=True)
+    else:
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+
+    page = request.GET.get('page')
+    page = page or 1
+    paginator = Paginator(products, 3)
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
+
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context = context)
+def HomeView(request):
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products,
+    }
+    return render(request, 'homepage/index.html', context=context)
+
